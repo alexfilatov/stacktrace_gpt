d964a74995544b15bb40849bcebe1f559db0c28f
Alex Filatov <alex@alexfilatov.com>
2023-11-02 22:53:35 +0000

    updated deps and bumped version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule StacktraceGpt.MixProject do
   def project do
     [
       app: :stacktrace_gpt,
-      version: "0.1.2",
+      version: "0.1.3",
       elixir: "~> 1.12",
       start_permanent: Mix.env() == :dev,
       deps: deps(),
